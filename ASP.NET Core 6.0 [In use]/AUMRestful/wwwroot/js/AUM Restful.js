3edca4588eb9476e9a2cf04ee92cdad95d67cda9
Eric Dee <eric_dee@outlook.com>
2022-03-21 07:54:41 -0600

    Changed some directory formatting.

---
+++ ASP.NET Core 6.0 [In use]/AUMRestful/wwwroot/js/AUM Restful.js
@@ -0,0 +1,55 @@
+﻿// Copyright aumono.com - 2022
+
+import "./json-formatter-js/dist/json-formatter.umd.js"
+
+let OutputFormatter;
+
+export class AUMRestful {
+
+    static GetViaHTTPS(urlBox, endpointBox, outputBox) {
+        let localURL = urlBox.value;
+        let localEndpoint = endpointBox.value;
+        let viewTesting = mockJSONResponse;
+        let outputType = viewTesting;
+
+        console.log("Starting a new HTTPS GET request:");
+
+        switch (outputType) {
+
+            case mockJSONResponse:
+                OutputFormatter = new JSONFormatter(outputType);
+                outputBox.appendChild(OutputFormatter.render());
+                OutputFormatter.openAtDepth(1);
+                break;
+
+            default:
+                fetch(outputType)
+                    .then(response => response.json())
+                    .then(responseAsJSON => {
+                        OutputFormatter = new JSONFormatter(responseAsJSON);
+                        outputBox.appendChild(OutputFormatter.render());
+                        OutputFormatter.openAtDepth(1);
+                    })
+                    .catch(e => {
+                        alert(e);
+                    });
+                break;
+        }
+    }
+}
+
+const Today = new Date();
+
+var mockJSONResponse =
+{
+    "Response": {
+        "Name": "Eric",
+        "Site": "aumono.com",
+        "Copyright": "2022",
+        "Art": "aumono.art",
+        "Blog": "aumono.blog"
+    },
+    "Type": "View",
+    "Location": "Output",
+    "Today's date": `${Today.getFullYear()}:${Today.getMonth()}-${Today.getDate()}`,
+}
---
+++ ASP.NET Core 6.0 [In use]/AUMRestful/wwwroot/js/Bar Main.js
@@ -0,0 +1,13 @@
+﻿// Copyright aumono.com - 2022
+
+import { AUMRestful } from "./AUM Restful.js"
+
+console.log("Bar Main.js loaded.");
+
+const URLInput = document.getElementById("URL-Input");
+
+const EndpointInput = document.getElementById("Endpoint-Input");
+
+const ResponseOutput = document.getElementById("Response-Content-Render");
+
+AUMRestful.GetViaHTTPS(URLInput, EndpointInput, ResponseOutput);
