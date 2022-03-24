9180e25a01885159938f06d1b61e59b79d58e44b
Eric Dee <eric_dee@outlook.com>
2022-03-24 16:14:51 -0600

    Adding more designs from the wireframe.

--- ASP.NET Core 6.0 [In use]/AUMRestful/wwwroot/js/Mode Bar.js
+++ ASP.NET Core 6.0 [In use]/AUMRestful/wwwroot/js/Mode Bar.js
@@ -22,16 +22,36 @@ AUMRestful.GetViaHTTPS(URLInput, EndpointInput, ResponseOutput);
 /*
  * 
  * 
-/* Options selector */
+/* Navigation button events */
 
-let TextToBaffle = document.getElementById("DefaultFieldText");
+const HomeButtonLeft = document.getElementById("Default-Home-Button-Left");
+const UpdatesButtonLeft = document.getElementById("Default-Updates-Button-Left");
+const SendButtonLeft = document.getElementById("Default-Send-Button-Left");
+const HomeNavButton = document.getElementById("Home-Navigator");
+const UpdatesNavButton = document.getElementById("Updates-Navigator");
+const SendNavButton = document.getElementById("Send-Navigator");
 
-const BaffledText = baffle(TextToBaffle);
+function BaffleText(textToBaffle) {
 
-BaffledText.set({
-    characters: '01',
-    speed: 4
-});
+    const BaffledText = baffle(textToBaffle);
 
-BaffledText.start()
-    .reveal(16, 256);
+    BaffledText.set({
+        characters: '01',
+        speed: 4
+    });
+
+    BaffledText.start()
+        .reveal(16, 256);
+}
+
+HomeNavButton.onclick = () => {
+    BaffleText(HomeButtonLeft);
+}
+
+UpdatesNavButton.onclick = () => {
+    BaffleText(UpdatesButtonLeft);
+}
+
+SendNavButton.onclick = () => {
+    BaffleText(SendButtonLeft);
+}
