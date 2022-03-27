61dafd1fd2a4e3216f7fda54609f8cd55bb75da4
Eric Dee <eric_dee@outlook.com>
2022-03-26 18:11:16 -0600

    Added comment formatting that keeps the code workable.

--- ASP.NET Core 6.0/AUMRestful/wwwroot/js/AUM-Dependencies/Modules/AUM DOM string functions.js
+++ ASP.NET Core 6.0/AUMRestful/wwwroot/js/AUM-Dependencies/Modules/AUM DOM string functions.js
@@ -4,34 +4,36 @@ export class AUMDOMStringFunctions {
     Strings = new Array()
     _Context = new Array()
 
-    /**
+    /****************
      * Stores strings as class members in a dynamic object array.
      * @param {string} stringName The string name/key.
      * @param {string} stringValue The string.
-     */
+     ****/
     AddString(stringName, stringValue) {
         this.Strings[stringName] = stringValue
     }
 
-    /**
+    /****************
      * Stores HTML elements to use as a context for retreiving strings.
      * @param {string} elementName The element name/key.
      * @param {object} elementContext The document element reference.
-     */
+     ****/
     AddElementToItsContext(elementName, elementContext) {
         this._Context[elementName] = elementContext
     }
 
-    /** Updates all strings pertaining to the document elements found in this._Context. */
+    /****************
+     * Updates all strings pertaining to the document elements found in this._Context. 
+     ****/
     UpdateContextStrings() {
         this.UpdateStringsByKeyValuePair(this._Context)
     }
 
-    /**
+    /****************
      * Adds strings to this.Strings from an object.
      * DEBUG?: This is only used for updating strings from this._Context via this.UpdateContextStrings above.
      * @param {any} objectToRead
-     */
+     ****/
     UpdateStringsByKeyValuePair(objectToRead) {
         for (const [key, value] of Object.entries(objectToRead)) {
             if (value.value) {
@@ -43,11 +45,11 @@ export class AUMDOMStringFunctions {
         }
     }
 
-    /**
+    /****************
      * Combines strings using a dynamic format.
      * @param {any} dynamicStringKey The master key. Can be an object, array, or string.
      * @param {string} decorator The filler to use between strings.
-     */
+     ****/
     CombineStringsWithADecorator(dynamicStringKey, decorator) {
         let result
         // If dynamicStringKey is an object in this._Context, this pulls its values from this.Strings.
@@ -88,12 +90,12 @@ export class AUMDOMStringFunctions {
         }
     }
 
-    /**
+    /****************
      * Decorates multiple strings from an object if it is found in this._Context.
      * DEBUG?: This functions requires the key's object to exist in this._Context.
-     * @param {any} objectKey The key for the object.
-     * @param {any} decorator The decorator character(s) to append between strings.
-     */
+     * @param {object} objectKey The key for the object.
+     * @param {string} decorator The decorator character(s) to append between strings.
+     ****/
     DecorateStringsFromAContextObject(objectKey, decorator) {
         let combinedString = ""
         for (const name of Object.keys(this._Context[objectKey])) {
@@ -113,12 +115,12 @@ export class AUMDOMStringFunctions {
         return combinedString
     }
 
-    /**
+    /****************
      * Decorates multiple strings from an array of keys or throws an error if any of the keys aren't found.
      * DEBUG?: This function requires all keys to be valid, due to the fact that it is meant for API endpoints.
-     * @param {any} keys The keys array for strings in this.Strings to decorate.
-     * @param {any} decorator The decorator character(s) to append between strings.
-     */
+     * @param {array} keys The keys array for strings in this.Strings to decorate.
+     * @param {string} decorator The decorator character(s) to append between strings.
+     ****/
     DecorateStringsFromAKeyArray(keys, decorator) {
         let combinedString = ""
         keys.forEach((name) => {
@@ -138,12 +140,12 @@ export class AUMDOMStringFunctions {
         return combinedString
     }
 
-    /**
+    /****************
      * Decorates a string from this.Strings using the concat function or throws an error if it isn't found.
      * It also prints undefined issues and errors for debugging.
-     * @param {any} stringKey The string key to search for.
-     * @param {any} decorator The decorator to character(s) append to the string.
-     */
+     * @param {string} stringKey The string key to search for.
+     * @param {string} decorator The decorator to character(s) append to the string.
+     ****/
     DecorateOneString(stringKey, decorator) {
         let stringContent = this.Strings[stringKey]
         if (stringContent === undefined) {
@@ -155,12 +157,16 @@ export class AUMDOMStringFunctions {
         }
     }
 
-    /** Console logs all the items in this.Strings. */
+    /****************
+     * Console logs all the items in this.Strings. 
+     ****/
     LogStrings() {
         console.log(this.Strings)
     }
 
-    /** Console logs all the items in this._Context. */
+    /****************
+     * Console logs all the items in this._Context. 
+     ****/
     LogContext() {
         console.log(this._Context)
     }
